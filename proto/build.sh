de9a6ef2aabd1da3319bf5f4149624b9dd52057f
Superjom <yanchunwei@outlook.com>
2016-05-24 21:12:34 +0800

    clean old code

--- proto/build.sh
+++
@@ -1,6 +0,0 @@
-set -e -x
-#!/bin/bash
-
-for file in `ls *.proto`; do
-    ~/project/hasky/third/local/bin/protoc $file --cpp_out ./
-done
--- unittest/utils/test_Vec.h
+++
@@ -1,5 +0,0 @@
-#include "utils/vec.h"
-
-TEST(Vec, init) {
-
-}
