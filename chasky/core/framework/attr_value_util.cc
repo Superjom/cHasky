6c5332fd2d3b4e1c027d62664eb15fd646f380c0
Superjom <superjom@gmail.com>
2016-06-11 10:35:59 +0800

    seperate StringToValue(str, dtype) to cpp

--- chasky/core/framework/attr_value_util.cc
+++ chasky/core/framework/attr_value_util.cc
@@ -1,5 +1,30 @@
 #include <iostream>
 #include "chasky/core/framework/attr_value_util.h"
 using namespace std;
-using namespace chasky;
 
+namespace chasky {
+
+void StringToValue(const StringPiece &str, DataType *dtype) {
+  if (str == "float")
+    *dtype = CH_FLOAT;
+  else if (str == "double")
+    *dtype = CH_DOUBLE;
+  else if (str == "int32")
+    *dtype = CH_INT32;
+  else if (str == "int64")
+    *dtype = CH_INT64;
+  else if (str == "uint32")
+    *dtype = CH_UINT32;
+  else if (str == "uint64")
+    *dtype = CH_UINT64;
+  else if (str == "string")
+    *dtype = CH_STRING;
+  else if (str == "vec_float")
+    *dtype = CH_VEC_FLOAT;
+  else if (str == "vec_double")
+    *dtype = CH_VEC_DOUBLE;
+  else if (str == "vec_int32")
+    *dtype = CH_VEC_INT32;
+}
+
+}
--- chasky/core/framework/attr_value_util.h
+++ chasky/core/framework/attr_value_util.h
@@ -85,27 +85,5 @@ inline void StringToValue(const StringPiece &str, float *val) {
   *val = stof(str.tostring(), nullptr);
 }
 
-inline void StringToValue(const StringPiece &str, DataType *dtype) {
-  if (str == "float")
-    *dtype = CH_FLOAT;
-  else if (str == "double")
-    *dtype = CH_DOUBLE;
-  else if (str == "int32")
-    *dtype = CH_INT32;
-  else if (str == "int64")
-    *dtype = CH_INT64;
-  else if (str == "uint32")
-    *dtype = CH_UINT32;
-  else if (str == "uint64")
-    *dtype = CH_UINT64;
-  else if (str == "string")
-    *dtype = CH_STRING;
-  else if (str == "vec_float")
-    *dtype = CH_VEC_FLOAT;
-  else if (str == "vec_double")
-    *dtype = CH_VEC_DOUBLE;
-  else if (str == "vec_int32")
-    *dtype = CH_VEC_INT32;
-}
 }
 #endif
