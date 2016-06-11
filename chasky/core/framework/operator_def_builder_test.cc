f5847c3568184a61b68213ed5fe5e98b443c40a7
Superjom <superjom@gmail.com>
2016-06-11 12:00:48 +0800

    add OperatorDefBuilder helper class

---
+++ chasky/core/framework/operator_def_builder_test.cc
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include "chasky/core/framework/attr_value_util.h"
+#include "chasky/core/framework/argument_def_builder.h"
+#include "chasky/core/framework/operator_def_builder.h"
+using namespace std;
+using namespace chasky;
+
+TEST(OperatorDefBuilder, init) {
+  OperatorDefBuilder builder;
+  builder.Name("layer1")
+      .Attr(NewAttrDefBuilder()
+                .Name("size")
+                .Type("int64")
+                .Doc("size of this layer")
+                .Finalize())
+
+      .Input(NewArgumentDefBuilder()
+                 .Name("input1")
+                 .Type("float_vec")
+                 .Doc("a float vector")
+                 .Finalize())
+
+      .Output(NewArgumentDefBuilder()
+                  .Name("output1")
+                  .Type("float_vec")
+                  .Doc("a float vector")
+                  .Finalize());
+
+  LOG(INFO) << "operator def:\n" << builder.DebugString();
+}
