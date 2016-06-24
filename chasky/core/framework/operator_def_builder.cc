4e51911baa488a3b93e8d5084f684d740c9c2e12
Superjom <superjom@gmail.com>
2016-06-24 09:55:31 +0800

    add default width on each operator

--- chasky/core/framework/operator_def_builder.cc
+++ chasky/core/framework/operator_def_builder.cc
@@ -4,7 +4,18 @@
 using namespace std;
 
 namespace chasky {
-OperatorDef& OperatorDefBuilder::Finalize() {
+
+// Set default width for each output
+OperatorDef &OperatorDefBuilder::Finalize() {
+  size_t default_width = 0;
+  if (def_.inputs_size() > 0) {
+    default_width = def_.inputs(0).shape().width();
+  }
+  for (size_t i = 0; i < def_.outputs_size(); i++) {
+    if (def_.outputs(i).shape().width() == 0) {
+      def_.mutable_outputs(i)->mutable_shape()->set_width(default_width);
+    }
+  }
   return def_;
 }
 
