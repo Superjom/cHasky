f7fd57f7a03456a46abd83751ca8c98a0d403f9e
Superjom <superjom@gmail.com>
2016-06-29 11:32:30 +0800

    enable protoc generate python scripts
    for all protobufs

--- compile_protobufs.sh
+++ compile_protobufs.sh
@@ -3,4 +3,5 @@ set -e -x
 
 mkdir -p build/protos
 
-protoc --proto_path=./ --cpp_out=build/protos chasky/core/framework/*.proto
+./thirdparty/local/bin/protoc --proto_path=./ --cpp_out=build/protos chasky/core/framework/*.proto
+./thirdparty/local/bin/protoc --proto_path=./ --python_out=build/protos chasky/core/framework/*.proto
