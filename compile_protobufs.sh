327de106ba73be5df3174aeeb5d814c2acfb05c6
Superjom <superjom@gmail.com>
2016-07-02 11:20:12 +0800

    add support for protobuf-python

--- compile_protobufs.sh
+++ compile_protobufs.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-set -e -x
+set -x
 
+rm -rf build/protos
 mkdir -p build/protos
 
 ./thirdparty/local/bin/protoc --proto_path=./ --cpp_out=build/protos chasky/core/framework/*.proto
 ./thirdparty/local/bin/protoc --proto_path=./ --python_out=build/protos chasky/core/framework/*.proto
+
+touch ./build/protos/chasky/__init__.py
+touch ./build/protos/chasky/core/__init__.py
+touch ./build/protos/chasky/core/framework/__init__.py
