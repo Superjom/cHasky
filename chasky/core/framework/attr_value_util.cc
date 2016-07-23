675a8e25a3ecd493cf0ddb7f26086b6581022631
Superjom <superjom@gmail.com>
2016-07-23 17:26:12 +0800

    clean types in attr_value_builder

--- chasky/core/framework/attr_value_util.cc
+++ chasky/core/framework/attr_value_util.cc
@@ -7,27 +7,22 @@ namespace chasky {
 void StringToValue(const StringPiece &str, DataType *dtype) {
   if (str == "float")
     *dtype = CH_FLOAT;
-  else if (str == "double")
-    *dtype = CH_DOUBLE;
-  else if (str == "int32")
-    *dtype = CH_INT32;
   else if (str == "int64")
     *dtype = CH_INT64;
-  else if (str == "uint32")
-    *dtype = CH_UINT32;
   else if (str == "uint64")
     *dtype = CH_UINT64;
   else if (str == "string")
     *dtype = CH_STRING;
-  else if (str == "vec_float")
-    *dtype = CH_VEC_FLOAT;
-  else if (str == "vec_double")
-    *dtype = CH_VEC_DOUBLE;
-  else if (str == "vec_int32")
-    *dtype = CH_VEC_INT32;
+  else if (str == "float_mat")
+    *dtype = CH_MAT_FLOAT;
+  else if (str == "float_mat_list")
+    *dtype = CH_MAT_FLOAT_LIST;
+  else {
+    LOG(FATAL) << "not supported type : " << str;
+  }
 }
 
-AttrDefBuilder& AttrDefBuilder::Value(const std::string &val) {
+AttrDefBuilder &AttrDefBuilder::Value(const std::string &val) {
   // TODO check value field is empty
   StringPiece value(val);
   // parse to value
@@ -55,6 +50,4 @@ AttrDefBuilder& AttrDefBuilder::Value(const std::string &val) {
 
   return *this;
 }
-
-
 }
