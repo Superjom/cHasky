8e44d4f71e3c8ebde9a1c3dcde78034b7078f2ea
Superjom <superjom@gmail.com>
2016-07-23 17:24:00 +0800

    add CH_USE_RESULT macro

--- chasky/common/macros.h
+++ chasky/common/macros.h
@@ -5,6 +5,8 @@
 #define CH_SCANF_ATTRIBUTE(string_index, first_to_check)                       \
   __attribute__((__format__(__scanf__, string_index, first_to_check)))
 
+#define CH_USE_RESULT __attribute__((warn_unused_result))
+
 // disable class's copy and assign function
 #define CH_DISALLOW_COPY_AND_ASSIGN(T)                                         \
   T(const T &) = delete;                                                       \
