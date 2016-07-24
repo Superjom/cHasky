83d87ebca7b7bfff1413a5541165527884256ccb
Superjom <superjom@gmail.com>
2016-07-24 12:42:19 +0800

    add kernel.h

---
+++ chasky/core/framework/kernel.h
@@ -0,0 +1,12 @@
+#ifndef CHASKY_CORE_FRAMEWORK_KERNEL_H_
+#define CHASKY_CORE_FRAMEWORK_KERNEL_H_
+namespace chasky {
+
+enum KernelType { CPU, GPU };
+
+class KernelInterface {
+public:
+}; // class KernelInterface
+
+} // namespace chasky
+#endif
