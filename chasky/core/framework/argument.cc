6ad50c6583dbf48ca9cbcded456902dee3efbba8
Superjom <superjom@gmail.com>
2016-08-02 19:20:36 +0800

    split Argument with ArgumentField
    create argument_field

--- chasky/core/framework/argument.cc
+++ chasky/core/framework/argument.cc
@@ -5,28 +5,6 @@
 using namespace std;
 
 namespace chasky {
-void ArgumentField::CopyFrom(const ArgumentField &other, bool is_ref) {
-  if (is_ref) {
-    uint64_val = other.uint64_val;
-    int64_val = other.int64_val;
-    float_mat_val = other.float_mat_val;
-  } else { // copy mode
-    UN_IMPLEMENTED
-  }
-}
-
-void ArgumentField::RealCopyFrom(const ArgumentField &other) {
-  // float_vec_val =
-  // std::make_shared<math::CpuFloatVector>(*other.float_vec_val);
-}
-
-bool ArgumentField::IsEmpty() const {
-  return float_val == nullptr && uint64_val == nullptr &&
-         float_mat_val == nullptr && float_mat_vals == nullptr &&
-         int64_vals == nullptr && uint64_vals == nullptr &&
-         float_vals == nullptr;
-}
-
 Argument::Argument(const Argument &other) : valid_(false) { Assign(other); }
 
 Status Argument::FromProto(const string &buffer) {
--- chasky/core/framework/argument.h
+++ chasky/core/framework/argument.h
@@ -12,63 +12,10 @@
 #include "chasky/core/common/eigen_matrix.h"
 #include "chasky/core/framework/kernel.h"
 #include "chasky/core/framework/argument.pb.h"
+#include "chasky/core/framework/argument_field.h"
 namespace chasky {
 using std::string;
 
-#define TYPE_GETTER(NAME, TYPE)                                                \
-  std::shared_ptr<TYPE> NAME;                                                  \
-  bool get(TYPE **x) {                                                         \
-    *x = NAME.get();                                                           \
-    return *x != nullptr;                                                      \
-  }                                                                            \
-  bool is_valid(TYPE *x) const { return NAME.get() != nullptr; }               \
-  void reset(std::shared_ptr<TYPE> x) { NAME = x; }                            \
-  void create_##NAME() { NAME.reset(new TYPE); };
-// Special methods for matrix
-#define TYPE_GETTER_MATRIX(NAME, TYPE)                                         \
-  void create_##NAME(const ArgumentDef::Shape &shape) {                        \
-    reset(std::make_shared<TYPE>(                                              \
-        std::make_pair(shape.width(), shape.height())));                       \
-  }
-
-// Storage of all kinds of data types, each Argument will have an ArgumentField
-// to store its value.
-// ArgumentField only support local memory storage.
-struct ArgumentField {
-  ArgumentField() {}
-
-  // Copy pointer
-  void CopyFrom(const ArgumentField &other, bool is_ref);
-
-  // Copy memory
-  void RealCopyFrom(const ArgumentField &other);
-
-  bool IsEmpty() const;
-
-  // single argument
-  TYPE_GETTER(string_val, std::string);
-  TYPE_GETTER(int64_val, int64_t);
-  TYPE_GETTER(uint64_val, uint64_t);
-  TYPE_GETTER(float_val, float);
-  TYPE_GETTER(float_mat_val, math::CpuFloatMatrix);
-  TYPE_GETTER_MATRIX(float_mat_val, math::CpuFloatMatrix);
-  // diffent types of raw array
-  TYPE_GETTER(string_vals, std::vector<std::string>);
-  TYPE_GETTER(int64_vals, std::vector<int64_t>);
-  TYPE_GETTER(uint64_vals, std::vector<uint64_t>);
-  TYPE_GETTER(float_vals, std::vector<float>);
-  // Use shapred_ptr to pass in arguments from other functions's output.
-  TYPE_GETTER(float_mat_vals,
-              std::vector<std::shared_ptr<math::CpuFloatMatrix>>)
-  // To support variadic number of arguments.
-  // TYPE_GETTER(float_vec_list, std::vector<math::CpuFloatVector>);
-}; // struct Argument
-#undef TYPE_GETTER
-
-typedef std::shared_ptr<ArgumentField> ArgFldPtr;
-
-// #define ARGUMENT_TYPE_GETTER(NAME, TYPE)                                       \
-//   std::shared_ptr<TYPE> NAME##() { return arg_field_->##NAME; }
 
 class Argument {
 public:
