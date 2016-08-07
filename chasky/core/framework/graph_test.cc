32fe25836346c3caa1cdadc78248ade48e63f679
Superjom <superjom@gmail.com>
2016-08-07 16:18:08 +0800

    move edge creation to class GraphTest

--- chasky/core/framework/graph_test.cc
+++ chasky/core/framework/graph_test.cc
@@ -7,68 +7,41 @@ namespace test {
 class GraphTest : public ::testing::Test {
 public:
   void SetUp() {
-    CreateGraphDef();
-
     graph_ = Graph::Create();
+
+    CreateGraphDef();
   }
 
   void CreateGraphDef() {
-    NodeDef node1 = NodeDefBuilder()
-                        .Name("data")
-                        .IsSource(true)
-                        .IsEnd(false)
-                        .Signature("AddN:float_mat")
-                        .Finalize();
+    edge_def = EdgeDefBuilder()
+                           .SrcNode("data")
+                           .SrcArg("output1")
+                           .TrgNode("node1")
+                           .TrgArg("input1")
+                           .Finalize();
+    edge_def1 = EdgeDefBuilder()
+                            .SrcNode("node1")
+                            .SrcArg("output3")
+                            .TrgNode("node1")
+                            .TrgArg("input1")
+                            .Finalize();
 
-    NodeDef node2 = NodeDefBuilder()
-                        .Name("node1")
-                        .IsSource(false)
-                        .IsEnd(false)
-                        .Signature("AddN:float_mat")
-                        .Finalize();
+    auto edge1 = std::make_shared<Edge>(edge_def, nullptr, nullptr);
+    EXPECT_TRUE(graph_->RegisterEdge(edge_def.signature_(), edge1).ok());
 
-    EdgeDef edge1 = EdgeDefBuilder()
-                        .SrcNode("data")
-                        .SrcArg("output1")
-                        .TrgNode("node1")
-                        .TrgArg("input1")
-                        .Finalize();
-
-    graph_def_ = GraphDefBuilder()
-                     .Name("graph1")
-                     .AddNodeDef(node1)
-                     .AddNodeDef(node2)
-                     .AddEdgeDef(edge1)
-                     .Finalize();
+    auto edge2 = std::make_shared<Edge>(edge_def1, nullptr, nullptr);
+    EXPECT_TRUE(graph_->RegisterEdge(edge_def1.signature_(), edge2).ok());
   }
 
 protected:
+  EdgeDef edge_def;
+  EdgeDef edge_def1;
   graph_ptr_t graph_;
-  GraphDef graph_def_;
 };
 
 TEST_F(GraphTest, setup) {}
 
 TEST_F(GraphTest, RegisterEdge) {
-
-  EdgeDef edge_def = EdgeDefBuilder()
-                         .SrcNode("data")
-                         .SrcArg("output1")
-                         .TrgNode("node1")
-                         .TrgArg("input1")
-                         .Finalize();
-  EdgeDef edge_def1 = EdgeDefBuilder()
-                          .SrcNode("node1")
-                          .SrcArg("output3")
-                          .TrgNode("node1")
-                          .TrgArg("input1")
-                          .Finalize();
-
-  auto edge1 = std::make_shared<Edge>(edge_def, nullptr, nullptr);
-  EXPECT_TRUE(graph_->RegisterEdge(edge_def.signature_(), edge1).ok());
-
-  auto edge2 = std::make_shared<Edge>(edge_def1, nullptr, nullptr);
-  EXPECT_TRUE(graph_->RegisterEdge(edge_def1.signature_(), edge2).ok());
 
   std::vector<edge_ptr_t> *edges1 = nullptr;
   EXPECT_TRUE(graph_->GetEdgesByTarget("node1:input1", &edges1).ok());
