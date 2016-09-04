f9e26470bb6673d658bf369a8fe212c9bf6b1533
Superjom <superjom@gmail.com>
2016-09-04 22:34:16 +0800

    add new graph based edge_lib

--- chasky/core/framework/graph.cc
+++ chasky/core/framework/graph.cc
@@ -1,81 +1,36 @@
-#include "chasky/core/common/status.h"
 #include "chasky/core/framework/graph.h"
+
 namespace chasky {
 
-std::unique_ptr<Graph> Graph::Create() {
-  return std::unique_ptr<Graph>(new Graph);
+Graph::Graph(GraphDef &def, PostBox *postbox)
+    : def_(def), node_lib_(postbox, &edge_lib_), postbox_(postbox) {
+  CH_CHECK_OK(CreateNodes());
+  CH_CHECK_OK(ConnectNodes());
 }
 
-std::shared_ptr<Argument> Graph::Param(const std::string &name) {
-  auto it = params_.find(name);
-  if (it == params_.end())
-    return nullptr;
-  return it->second;
-}
-
-Status Graph::RegisterEdge(StringPiece sign, edge_ptr_t edge) {
+Status Graph::CreateNodes() {
   Status status;
-  DLOG(INFO) << "register edge called " << sign;
-  std::string signature = sign.tostring();
-  auto it = edges_.find(signature);
-  CH_STEST_RETURN2(it == edges_.end(), error::INVALID_ARGUMENT,
-                   "an edge called %s already exists", signature.c_str());
-  CH_STEST_RETURN2(edges_.insert({signature, edge}).second, error::UNKNOWN,
-                   "edges_ insert map error");
-
-#define REGISTER_EDGE(EDGES, KEY)                                              \
-  if (EDGES.find(KEY) == EDGES.end()) {                                        \
-    EDGES.emplace(KEY, std::vector<edge_ptr_t>());                             \
-  }                                                                            \
-  EDGES[KEY].push_back(edge);
-
-  REGISTER_EDGE(src2edges_, edge->SrcSign().tostring())
-  REGISTER_EDGE(trg2edges_, edge->TrgSign().tostring())
-#undef REGISTER_EDGE
-
+  CHECK(node_lib_.IsEmpty()) << "duplicate creating nodes";
+  for (const auto &node_def : def_.nodes()) {
+    LOG(INFO) << strings::Printf("create node: %s[%s]", node_def.name().c_str(),
+                                 node_def.signature().c_str());
+    CH_CHECK_OK(node_lib_.RegisterCreate(node_def.name(), node_def));
+  }
   return status;
 }
 
-Status Graph::GetEdgeBySign(StringPiece sign, Edge **edge) {
+Status Graph::ConnectNodes() {
   Status status;
-  std::string signature = sign.tostring();
-  auto it = edges_.find(signature);
-  CH_STEST_RETURN2(it != edges_.end(), error::OUT_OF_RANGE,
-                   "no edges exists called [%s]", signature.c_str());
-  *edge = it->second.get();
-  return status;
-}
+  CH_STEST_RETURN2(!node_lib_.IsEmpty(), error::UNKNOWN,
+                   "no nodes are found in graph, create edges failed.");
+  CHECK(edge_lib_.IsEmpty()) << "duplicate create edges";
 
-Status Graph::GetEdgesByTarget(const std::string &signature,
-                               std::vector<edge_ptr_t> **edges) {
-  Status status;
-  auto it = trg2edges_.find(signature);
-  CH_STEST_RETURN2(it != trg2edges_.end(), error::OUT_OF_RANGE,
-                   "no key called %s in trg2edges_", signature.c_str());
-  *edges = &it->second;
-  return status;
-}
+  for (auto &e : *def_.mutable_edges()) {
+    auto sign = EdgeLib::CreateKey(e);
+    e.set_signature_(sign);
+    edge_lib_.Register(sign);
+  }
 
-Status Graph::GetEdgeByTarget(const std::string &signature, edge_ptr_t *edge) {
-  std::vector<edge_ptr_t> *edges;
-  Status status = GetEdgesByTarget(signature, &edges);
-  if (!status.ok())
-    return status;
-  CH_STEST_RETURN2(
-      edges->size() == 1, error::OUT_OF_RANGE,
-      "there are [%lu] edges registered by target [%s] is registered",
-      edges->size(), signature.c_str());
-  *edge = edges->at(0);
-  return status;
-}
-
-Status Graph::GetEdgesBySource(const std::string &signature,
-                               std::vector<edge_ptr_t> **edges) {
-  Status status;
-  auto it = src2edges_.find(signature);
-  CH_STEST_RETURN2(it != src2edges_.end(), error::OUT_OF_RANGE,
-                   "no key called %s exists in src2edges_", signature.c_str());
-  *edges = &it->second;
   return status;
 }
 
--- chasky/core/framework/graph.h
+++ chasky/core/framework/graph.h
@@ -1,79 +1,39 @@
 #ifndef CHASKY_CORE_FRAMEWORK_GRAPH_H_
 #define CHASKY_CORE_FRAMEWORK_GRAPH_H_
 #include <unordered_map>
+
 #include "chasky/core/framework/node.h"
 #include "chasky/core/framework/graph.pb.h"
+#include "chasky/core/runtime/edge_lib.h"
+#include "chasky/core/runtime/node_lib.h"
+#include "chasky/core/runtime/parameter_lib.h"
+
 namespace chasky {
-class Graph;
-using graph_ptr_t = std::shared_ptr<Graph>;
+using std::string;
 
 class Graph {
 public:
-  typedef std::unordered_map<std::string, node_ptr_t> nodes_t;
-  typedef std::unordered_map<std::string, edge_ptr_t> edges_t;
-  typedef std::unordered_map<std::string, std::shared_ptr<Argument>> args_t;
-
-  static std::unique_ptr<Graph> Create();
-
-  // Get a parameter by name
-  std::shared_ptr<Argument> Param(const std::string &name);
-
-  // Get a edge point to target Argument whose `signature` is like
-  // "{node}:{arg_name}".
-  // NOTE slow version.
-  Status GetEdgeByTarget(const std::string &signature, edge_ptr_t *edge);
-
-  // Get all edges point to target Node identified by `signature`.
-  // The `signature` should be format like "{node}:{arg_name}"
-  // NOTE slow version.
-  Status GetEdgesByTarget(const std::string &signature,
-                          std::vector<edge_ptr_t> **edges);
-
-  // Get all edges point from Source Node which identified by `signature`.
-  Status GetEdgesBySource(const std::string &signature,
-                          std::vector<edge_ptr_t> **edges);
-
-  // Register a parameter by name and definiton.
-  // NOTE not thread-safe.
-  Status RegisterParameter(const std::string &name, const ArgumentDef &def) {
-    Status status;
-    auto it = params_.find(name);
-    if (it != params_.end()) {
-      status.Update(error::INVALID_ARGUMENT,
-                    strings::Printf("duplicate register parameter called %s",
-                                    name.c_str()));
-    } else if (!params_.insert({name, std::make_shared<Argument>(&def)})
-                    .second) {
-      status.Update(error::UNKNOWN,
-                    strings::Printf("parameter %s insert error", name.c_str()));
-    }
-    return status;
+  static std::unique_ptr<Graph> Create(GraphDef &def, PostBox *postbox) {
+    return std::unique_ptr<Graph>(new Graph(def, postbox));
   }
 
-  // Register an edge by signature like "{node}:{arg_name}".
-  Status RegisterEdge(StringPiece sign, edge_ptr_t edge);
-  Status GetEdgeBySign(StringPiece sign, Edge **edge);
+  string DebugString() const;
 
-  // getters
-  const nodes_t &Nodes() const { return nodes_; }
-  nodes_t *mutable_nodes() { return &nodes_; }
-  const edges_t &Edges() const { return edges_; }
-  edges_t *mutable_edges() { return &edges_; }
-  TaskType TaskType() const { return task_type_; }
-  enum TaskType *mutable_tasktype() { return &task_type_; }
+protected:
+  Graph(GraphDef &def, PostBox *postbox);
+
+  Status CreateNodes();
+
+  Status ConnectNodes();
 
 private:
-  std::unordered_map<std::string, node_ptr_t> nodes_;
-  std::unordered_map<std::string, edge_ptr_t> edges_;
-  std::unordered_map<std::string, std::vector<edge_ptr_t>> trg2edges_;
-  std::unordered_map<std::string, std::vector<edge_ptr_t>> src2edges_;
-  // Model parameters, shared by all nodes in a graph.
-  // Key are "%s:%s" % (node.name, param.name)
-  // The parameters are registered by Nodes.
-  std::unordered_map<std::string, std::shared_ptr<Argument>> params_;
-  // Mark current task, forward or backward.
-  enum TaskType task_type_;
+  EdgeLib edge_lib_;
+  NodeLib node_lib_;
+  ParameterLib param_lib_;
+  GraphDef def_;
+  PostBox *postbox_;
 };
 
 } // namespace chasky
+
 #endif
