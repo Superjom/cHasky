be4a41d924248017a52a6b0cb0adef13adef2cd4
Superjom <superjom@gmail.com>
2016-09-07 10:53:44 +0800

    add edge_def_builder.h

---
+++ chasky/core/framework/edge_def_builder.h
@@ -0,0 +1,38 @@
+#ifndef CHASKY_CORE_FRAMEWORK_EDGE_DEF_BUILDER_H_
+#define CHASKY_CORE_FRAMEWORK_EDGE_DEF_BUILDER_H_
+
+#include "chasky/core/framework/graph.pb.h"
+
+namespace chasky {
+using namespace std;
+class EdgeDefBuilder {
+public:
+  explicit EdgeDefBuilder() {}
+
+  EdgeDefBuilder &SrcNode(const string &x) {
+    *def_.mutable_src_node() = x;
+    return *this;
+  }
+
+  EdgeDefBuilder &SrcArg(const string &x) {
+    *def_.mutable_src_arg() = x;
+    return *this;
+  }
+
+  EdgeDefBuilder &TrgNode(const string &x) {
+    *def_.mutable_trg_node() = x;
+    return *this;
+  }
+
+  EdgeDefBuilder &TrgArg(const string &x) {
+    *def_.mutable_trg_arg() = x;
+    return *this;
+  }
+
+  EdgeDef &Finalize() { return def_; }
+
+private:
+  EdgeDef def_;
+};
+} // namespace chasky
+#endif
